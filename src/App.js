6670fa43a58552dd6f16960a09a23f563c9694a9
rehmangour04 <rehmangour04@gmail.com>
2022-01-18 16:11:31 +0530

    the checkout page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 /** @format */
 
 import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>hey rehman gour</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -2,18 +2,42 @@
 
 import React from "react";
 import "./Header.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import SearchIcon from "@mui/icons-material/Search";
 const Header = () => {
   return (
     <div className="header">
       <img
         className="header__logo"
-        src="http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG "
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png "
+        alt="amazon-png-loaded"
       />
       <div className="header__search">
         <input className="header__searchInput" type="text" />
+        <SearchIcon className=" header__searchIcon" />
       </div>
-      ""
-      <div className="header__nav"></div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne"> Hello Guest</span>
+          <span className="header__optionLineTwo"> Sign In</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne"> Returns </span>
+          <span className="header__optionLineTwo"> &Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne"> Your</span>
+          <span className="header__optionLineTwo"> Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
     </div>
   );
 };
