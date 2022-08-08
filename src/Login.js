b37e9e84a92341e59f0b2a40b5f3793cdef83e64
rehmangour04 <rehmangour04@gmail.com>
2022-08-08 12:43:38 +0530

    deploy on heroku

--- src/Login.js
+++ src/Login.js
@@ -20,11 +20,21 @@ const Login = () => {
           <h5>E-mail</h5>
           <input type="text"></input>
           <h5>Password</h5>
-          <input type="text"></input>
+          <input type="password"></input>
+          {/* <br /> */}
+          <button className="login__signInButton"> Sign In</button>
         </form>
+        <p>
+          By signing-in you agree to the AMAZON CLONE Conditions of Use & Sale.
+          Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
-};
+}; 
 
 export default Login;
